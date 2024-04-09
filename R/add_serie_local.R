ebecdcd827b9e7884b4b0cb6e04bd7e469319005
Fabián Salazar, Miguel <mfabian@economia.gob.es>
2024-04-09 18:58:14 +0200

    20240409 add_serie_local.R, update_local_to_server.R: funciones para añadir series localmente y actualizar de local a servidor todo el repositorio

--- R/add_serie_local.R
+++ R/add_serie_local.R
@@ -1,6 +1,6 @@
-#' Add time series to the Tesoro series repository
+#' Add time series to the Tesoro series local repository
 #'
-#' This function adds a wide-format df storing a serie to the catalog of series and to the data repository of tesoroseries.
+#' This function locally adds a wide-format dataframe storing a serie, to the catalog of series and to the data repository of tesoroseries.
 #' @param .df dataframe containing the serie to be added
 #' @param .codigo code of the serie
 #' @param .descripcion  description of the serie
@@ -9,9 +9,7 @@
 #' @examples
 #' get_series()
 #' s@export
-
-
-add_serie <- function(.df,
+add_serie_local <- function(.df,
                       .codigo,
                       .descripcion,
                       .fichero="",
@@ -27,42 +25,30 @@ add_serie <- function(.df,
                       forceoverwrite = FALSE) {
   
   
-
-  
   .datos_server_path <- getOption("datos_server_path")
   
   .datos_path <- gsub("\\\\",
-                     "/",
-                     tools::R_user_dir("tesoroseries", which = "data"))
+                      "/",
+                      tools::R_user_dir("tesoroseries", which = "data"))
   
   if(is.null(.df) | ncol(.df) > 2) {
     message("Dataframe cannot have more than two columns.")
     return(NULL)
   }
   
-  tryCatch({
-    # to avoid possible consistency problems, we always try to update catalogo_db.feather IN THE SERVER, not the local copy
-    zip_file_server_path <- gsub("/",
-                                           "\\\\",
-                                           paste0(.datos_server_path, "tesoroseries.zip"))
+  existing_catalogo <- tryCatch({
     
-    temp_dir_catalog_db <- tempdir()
-    
-    zip::unzip(zipfile=zip_file_server_path,
-               files="catalogo_db.feather",
-               exdir=temp_dir_catalog_db)
-    
-    existing_catalogo_path <- paste0(temp_dir_catalog_db, "\\catalogo_db.feather")
-    
-    existing_catalogo <- feather::read_feather(existing_catalogo_path)
-    },
-    error = function(e) {
-      message("Cannot read catalogo_db.feather")
-      message("Path to file: ", .datos_server_path)
-      message("Error: ", e)
-      return(NULL)
-      })
-  
+    existing_catalogo_path <- paste0(.datos_path, "/catalogo_db.feather")
+
+    feather::read_feather(existing_catalogo_path)
+  },
+  error = function(e) {
+    message("Cannot read local copy of catalogo_db.feather")
+    message("Path to file: ", .datos_server_path)
+    message("Error: ", e)
+    return(NULL)
+  })
+
   if(nrow(existing_catalogo) > 0) { # check that catalogo_db.feather is not an empty one, otherwise it's necessarily a new serie to add
     existing_entry <- existing_catalogo |>
       dplyr::filter(nombre == paste0("TESORO_", 
@@ -72,7 +58,7 @@ add_serie <- function(.df,
     if (nrow(existing_entry) == 0) {
       message("New serie ", .codigo, ".")
       
-    # catalogo_db.feather already contains a serie with the same name as the one to be added
+      # catalogo_db.feather already contains a serie with the same name as the one to be added
     } else if(((existing_entry$nombre |> 
                 unique()) == paste0("TESORO_", .codigo |> 
                                     stringr::str_remove("TESORO_"))) | 
@@ -90,8 +76,8 @@ add_serie <- function(.df,
   # df con los datos a guardar
   df_to_save <- .df |>
     tidyr::pivot_longer(cols=-c("fecha"),
-                 names_to="nombres",
-                 values_to="valores") |>
+                        names_to="nombres",
+                        values_to="valores") |>
     dplyr::mutate(codigo = paste0("TESORO_", .codigo),
                   nombres = .descripcion,
                   fichero = paste0(.datos_server_path, .codigo, ".feather"),
@@ -109,32 +95,24 @@ add_serie <- function(.df,
   feather::write_feather(df_to_save,
                          paste0(.datos_path, "/", .codigo, ".feather"))
   
-  # guardar en SERVIDOR
-  # zip::zip_append(zipfile=zip_file_server_path,
-  #                 files=paste0(.datos_path, .codigo, ".feather"))
-  
-  zip(zipfile=zip_file_server_path,
-                  files=paste0(.datos_path, "/", .codigo, ".feather"),
-                  extras = '-ju')
-  
-    # entrada de catálogo a añadir
+  # entrada de catálogo a añadir
   .entrada_catalogo <- dplyr::tibble(nombre =  paste0("TESORO_", .codigo),
-                              numero = "",
-                              alias=.descripcion,
-                              fichero = paste0(.datos_server_path, .codigo, ".feather"),
-                              descripcion=.descripcion, 
-                              tipo="",
-                              unidades=.unidades,
-                              decimales = .decimales,
-                              exponente = .exponente,
-                              numero_observaciones=nrow(.df),
-                              descripcion_unidades_exponente = .descripcion_unidades_exponente,
-                              fecha_primera_observacion=min(.df$fecha),
-                              fecha_ultima_observacion=max(.df$fecha),
-                              titulo="",
-                              frecuencia = .frecuencia,
-                              fuente = .fuente,
-                              notas = .notas)
+                                     numero = "",
+                                     alias=.descripcion,
+                                     fichero = paste0(.datos_server_path, .codigo, ".feather"),
+                                     descripcion=.descripcion, 
+                                     tipo="",
+                                     unidades=.unidades,
+                                     decimales = .decimales,
+                                     exponente = .exponente,
+                                     numero_observaciones=nrow(.df),
+                                     descripcion_unidades_exponente = .descripcion_unidades_exponente,
+                                     fecha_primera_observacion=min(.df$fecha),
+                                     fecha_ultima_observacion=max(.df$fecha),
+                                     titulo="",
+                                     frecuencia = .frecuencia,
+                                     fuente = .fuente,
+                                     notas = .notas)
   
   catalogo <- existing_catalogo |>
     dplyr::bind_rows(.entrada_catalogo) |>
@@ -146,14 +124,16 @@ add_serie <- function(.df,
                          path=paste0(.datos_path, "/catalogo_db.feather"))
   
   
-  # save catalogo_db.feather in SERVER
-  message("Adding serie to the series in server...")
-
+  # # save catalogo_db.feather in SERVER
+  # message("Adding serie to the series in server...")
+  # # zip::zip_append(zipfile=zip_file_server_path,
+  # #                 files=paste0(.datos_path, "/catalogo_db.feather"), 
+  # #                 extras = '-j')
   
-  zip(zipfile=zip_file_server_path,
-      files=paste0(.datos_path, "/catalogo_db.feather"), 
-      extras = '-j')
-
+  # zip(zipfile=zip_file_server_path,
+  #     files=paste0(.datos_path, "/catalogo_db.feather"), 
+  #     extras = '-j')
+  
   return(.entrada_catalogo)
   
 }
---
+++ R/update_local_to_server.R
@@ -0,0 +1,48 @@
+#' Updates the server database from the local one.
+#'
+#' This function copies the local database to the server. It should be used in conjunction with add_serie_local when 
+#' adding series in bulk, instead of making multiple calls to add_serie()
+#' @keywords update local to server
+#' @examples
+#' update_local_to_server()
+#' s@export
+update_local_to_server <- function() {
+  
+  .datos_server_path <- getOption("datos_server_path")
+  .datos_path <- gsub("/",
+                      "\\\\",
+                      tools::R_user_dir("tesoroseries", which = "data"))
+  
+  
+  zip_file_server_path <- paste0(.datos_server_path, 
+                                 "tesoroseries.zip")
+  
+  
+  feathers_files_list_local <- fs::dir_ls(path=.datos_path,
+                                          glob="*.feather")
+  
+
+  if(!file.exists(zip_file_server_path)) {
+    stop("tesoroseries: download_series_full(): tesoroseries.zip does not exist in the server.")
+  }
+  
+  # zipping local data directory to tesoroseries.zip in server
+  tryCatch({
+    
+    zip(zipfile=zip_file_server_path,
+        files=feathers_files_list_local,
+        extras = '-j')
+    
+    zip(zipfile=zip_file_server_path,
+        files=paste0(.datos_path, "/catalogo_db.feather"), 
+        extras = '-j')
+    
+  },
+  error = function(e) {
+    stop("update_local_to_server: ", e)
+  })
+  
+  message("Series successfully updated locally.")
+  
+  
+}
